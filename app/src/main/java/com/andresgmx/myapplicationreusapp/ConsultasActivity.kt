91182236c5499254b606a3c3040833764ec881a4
Andres <gmxandres11@gmail.com>
2024-06-30 16:25:04 -0500

    ventana historial estatica

--- app/src/main/java/com/andresgmx/myapplicationreusapp/ConsultasActivity.kt
+++ app/src/main/java/com/andresgmx/myapplicationreusapp/ConsultasActivity.kt
@@ -8,14 +8,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.andresgmx.myapplicationreusapp.db.dbConnection
 
 class ConsultasActivity : AppCompatActivity() {
     private lateinit var btnHorario: Button
+    private lateinit var btnHistorial: Button
+    private lateinit var dbConnection: dbConnection
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_consultas)
-
+        dbConnection = dbConnection(this)
         setupActionBar()
         initcomponents()
         initListeners()
@@ -26,9 +29,14 @@ class ConsultasActivity : AppCompatActivity() {
             insets
         }
     }
+    private fun navigateTo(activityClass: Class<*>) {
+        val intent = Intent(this, activityClass)
+        startActivity(intent)
+    }
 
     private fun initcomponents() {
         btnHorario = findViewById(R.id.btnHorario)
+        btnHistorial = findViewById(R.id.btnHistorial)
     }
 
     private fun initListeners() {
@@ -37,6 +45,10 @@ class ConsultasActivity : AppCompatActivity() {
                 "https://www.amb.gov.co/punto-limpio-metropolitano-de-bucaramanga-y-14-sitios-mas-en-santander-estan-recibiendo-los-residuos-pos-consumo-hoy-y-manana/"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlHorario))
             startActivity(intent)
+        }
+        btnHistorial.setOnClickListener {
+
+            navigateTo(HistorialReciclaje::class.java)
         }
     }
 
@@ -51,4 +63,5 @@ class ConsultasActivity : AppCompatActivity() {
         onBackPressedDispatcher.onBackPressed()
         return true
     }
+
 }
---
+++ app/src/main/java/com/andresgmx/myapplicationreusapp/HistorialReciclaje.kt
@@ -0,0 +1,33 @@
+package com.andresgmx.myapplicationreusapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HistorialReciclaje : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_historial_reciclaje)
+        setupActionBar()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    private fun setupActionBar() {
+        supportActionBar?.apply {
+            title = "Regresar"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
+    }
+}
